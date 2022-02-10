ba874deca1731f510057572ca18d47894126ecbf
CdrcMag <44393506+CdrcMag@users.noreply.github.com>
2022-02-11 00:22:24 +0100

    Audio

--- Projet_One/Assets/2 - Scripts/Manager_Audio.cs
+++ Projet_One/Assets/2 - Scripts/Manager_Audio.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
-[RequireComponent(typeof(Manager_Audio))]
+//[RequireComponent(typeof(Manager_Audio))]
 public class Manager_Audio : MonoBehaviour
 {
     public static Manager_Audio Instance;
@@ -13,6 +15,9 @@ public class Manager_Audio : MonoBehaviour
     {
         Instance = this;
         _audioSource = GetComponent<AudioSource>();
+
+        if (PlayerPrefs.GetInt("Sound_State") == 1) EnableAudio();
+        if (PlayerPrefs.GetInt("Sound_State") == 0) MuteAudio();
     }
 
     public AudioClip[] fx;
@@ -29,7 +34,69 @@ public class Manager_Audio : MonoBehaviour
         _audioSource.PlayOneShot(fx_movement[Random.Range(0, fx_movement.Length)], volume);
     }
 
+    public void MuteAudio()
+    {
+        _audioSource.enabled = false;
+        PlayerPrefs.SetInt("Sound_State", 0);
+        SetStuff(false);
+    }
 
+    public void EnableAudio()
+    {
+        _audioSource.enabled = true;
+        PlayerPrefs.SetInt("Sound_State", 1);
+        SetStuff(true);
+    }
+
+    public TextMeshProUGUI OnText;
+    public TextMeshProUGUI OffText;
+    public Transform img;
+
+    //0 et -72
+    private void SetStuff(bool val)
+    {
+        if(!val)//false = sound off
+        {
+            OnText.color = Color.black;
+            OffText.color = Color.white;
+            StartCoroutine(MoveImg(10));
+        }
+        if (val)//true = sound on
+        {
+            OnText.color = Color.white;
+            OffText.color = Color.black;
+            StartCoroutine(MoveImg(-10));
+        }
+
+    }
+
+    private IEnumerator MoveImg(float speed)
+    {
+        if(speed > 0)
+        {
+            while (img.localPosition.x <= 14)
+            {
+                img.localPosition = new Vector2(img.localPosition.x + speed, img.localPosition.y);
+                yield return null;
+            }
+
+            img.localPosition = new Vector2(14, img.localPosition.y);
+
+        }
+
+        if (speed < 0)
+        {
+            while (img.localPosition.x >= -72)
+            {
+                img.localPosition = new Vector2(img.localPosition.x + speed, img.localPosition.y);
+                yield return null;
+            }
+
+            img.localPosition = new Vector2(-72, img.localPosition.y);
+
+        }
+
+    }
 
 
 
